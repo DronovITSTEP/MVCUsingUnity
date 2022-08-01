2ba4f183c65c482e870c7921b3194ad6f788d4a3
ApxNord <nord-life17@yandex.ru>
2022-08-01 10:23:26 +0300

    Добавьте файлы проекта.

---
+++ MVCUsingUnity/UnityConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+
+namespace MVCUsingUnity
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            DependencyResolver.SetResolver(new UnityDependencyResolver
+                (container));
+        }
+    }
+}
